ae6cd48c08a342978a9ae7d3b90af3172b981d7b
James Gladden <jamesgladden93@gmail.com>
2026-06-22 17:04:15 -0500

    INTENTIONAL REGRESSION: revert S3 SSE to AES256 (triggers GAP-01 Conftest failure)

--- terraform/s3-hardening.tf
+++ terraform/s3-hardening.tf
@@ -4,7 +4,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "uploads" {
   rule {
     apply_server_side_encryption_by_default {
       kms_master_key_id = aws_kms_key.phi.arn
-      sse_algorithm     = "aws:kms"
+      sse_algorithm     = "AES256"
     }
   }
 }
